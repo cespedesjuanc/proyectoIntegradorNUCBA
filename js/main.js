34e1e0fd414ad2cb2a8c348db8ef978e7ae87ff6
cespedesjuanc <cespedesjuanc@MacBook-Pro-de-Juan.local>
2022-06-24 23:30:41 -0300

    se agrego seccion de Log in

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* Modal Register*/
 const openModalRegister = document.querySelector(".btn-register-class");
 const modalRegister = document.querySelector(".modalReg");
 const closeModalRegister = document.querySelector(".modal__closeReg");
@@ -10,4 +11,19 @@ openModalRegister.addEventListener("click", (e) => {
 closeModalRegister.addEventListener("click", (e) => {
   e.preventDefault();
   modalRegister.classList.remove("modal--show");
+});
+
+/* Modal Log in */
+const openModalLogin = document.querySelector(".btn-login-class");
+const modalLog = document.querySelector(".modalLog");
+const closeModalLog = document.querySelector(".modal__closeLog");
+
+openModalLogin.addEventListener("click", (e) => {
+  e.preventDefault();
+  modalLog.classList.add("modal--showLog");
+});
+
+closeModalLog.addEventListener("click", (e) => {
+  e.preventDefault();
+  modalLog.classList.remove("modal--showLog");
 });
